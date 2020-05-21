aeea1d74b87bb3c49a7cf893a0e30960bad1bc3a
njferrari <njferrari14@gmail.com>
2020-05-21 15:01:55 -0500

    finished prototype

--- builds/public_html/js/scripts.js
+++ builds/public_html/js/scripts.js
@@ -1,17 +1,23 @@
-var acc = document.getElementsByClassName("learn");
+var acc = document.getElementsByClassName('learn');
+var boxes = document.querySelectorAll('.training-box');
 var i;
+var service = document.getElementById('services');
+var width = updateSize();
+window.addEventListener('resize', function() {
+	width = updateSize();
+});
+setListeners();
 
+function updateSize() {
+	return window.outerWidth;
+}
 
-		console.log(acc);
-
-run();
-
-function run() {
-		for (i = 0; i < acc.length; i++) {
-	  acc[i].addEventListener("click", function() {
-	    /* Toggle between adding and removing the "active" class,
+function setListeners() {
+	for (i = 0; i < acc.length; i++) {
+	  acc[i].addEventListener('click', function() {
+	    /* Toggle between adding and removing the 'active' class,
 	    to highlight the button that controls the panel */
-	    this.classList.toggle("active");
+	    this.classList.toggle('active');
 
 	    /* Toggle between hiding and showing the active panel */
 	    var panel = this.previousElementSibling;
@@ -19,11 +25,11 @@ function run() {
 	    if (panel.style.maxHeight) {
 	      panel.style.maxHeight = null;
 	      panel.style.marginBottom = null;
-	      learnMore.innerHTML = "Learn More";
+	      learnMore.innerHTML = 'Learn More';
 	    } else {
-	      panel.style.maxHeight = panel.scrollHeight + "px";
+	      panel.style.maxHeight = panel.scrollHeight + 'px';
 	      panel.style.marginBottom = '20px';
-	      learnMore.innerHTML = "Hide";
+	      learnMore.innerHTML = 'Hide';
 	    }
 	  });
 	}
